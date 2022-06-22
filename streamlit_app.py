70c27b00c03ffb0bcebe8bd1b73db3d482064352
kmarinko <marinko.kocevski@gmail.com>
2022-06-22 14:09:15 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,8 +32,8 @@ try:
         from_fn = get_fruit_data(fruit_choice)
         streamlit.dataframe(from_fn)
 
-except URLError as e:
-    streamlit.error()
+#except URLError as e:
+#    streamlit.error()
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from fruit_load_list")
