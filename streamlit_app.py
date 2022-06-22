8cfa121668cf4025101c10bb084946868620ab3b
kmarinko <marinko.kocevski@gmail.com>
2022-06-22 14:06:58 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,8 +32,8 @@ try:
         from_fn = get_fruit_data(fruit_choice)
         streamlit.dataframe(from_fn)
 
-except URLError as e:
-    streamlit.error()
+#except URLError as e:
+#    streamlit.error()
     
 #add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
 #streamlit.write('Thanks for adding ', add_my_fruit)
